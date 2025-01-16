40237dcf19c37490ed938189cf545787aabb4354
DevinShelton-1 <dshelton7972@floridapoly.edu>
2025-01-16 13:29:46 +0000

    Code First Program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,24 @@
+// This is the first program in C.
+// It prints a welcome message to the screen.
+
+// - one-line comment
+/*
+The
+Multi
+Line
+Comment
+of
+all
+time
+*/
+
+#include <stdio.h>
+
+//Create a main function
+int main() {
+    // Call theprintf function
+    printf("We\nare\nprogamming\nnow.\n");
+    printf("IT'S\nHAPPENING\nAGAIN!\n");
+
+    return 0;
+}
